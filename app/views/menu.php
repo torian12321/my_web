11969ffa39ef892c1e1babb91175cf089af16266
torian12321 <aitorpalomares@hotmail.com>
2016-09-07 00:19:43 +0100

    menu

--- app/views/menu.php
+++ app/views/menu.php
@@ -1,7 +1,19 @@
-<div class="menu-closer" data-activate="this"></div>
+<div class="menu-opener">
+    <div></div>
+    <div></div>
+    <div></div>
+</div>
 
-<div class="wrap">
-	<div class="content">
-		text on the menu
-	</div>
+
+<div class="menu-wrap">
+    <div class="menu-content">
+    <div class="img"></div>
+        <ul>
+            <li>Option 1</li>
+            <li>Option 2</li>
+            <li>Option 3</li>
+            <li>Option 4</li>
+            <li>Option 5</li>
+        </ul>
+    </div>
 </div>
