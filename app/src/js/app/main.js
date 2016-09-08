8f7ba72d739237ba711520dce6cbfe7913bd272c
torian12321 <aitorpalomares@hotmail.com>
2016-09-08 02:35:08 +0100

    pure vanilla js. No jquery anymore

--- app/src/js/app/main.js
+++ app/src/js/app/main.js
@@ -2,13 +2,25 @@ var
 _doc = document,
 _win = window,
 init = function(){
-	fancy_functions();
-    fitContentOnScreen();
+    menu_func();
+    smoothScrolling();
+    //fitContentOnScreen();
+    fancy_functions();
 },
 resize = function(){
-    fitContentOnScreen();
+    //fitContentOnScreen();
+},
+menu_func = function(){
+    var
+    opener = _doc.querySelector('.menu-opener'),
+    menu   = _doc.querySelector('.menu-wrap');
+
+    opener.addEventListener('click', function(){
+        opener.classList.toggle('active');
+        menu.classList.toggle('active');
+    });
 };
 
 
-$(_doc).ready(init);
-$(_win).resize(resize);
+_doc.addEventListener('DOMContentLoaded', init);
+_win.addEventListener('resize', resize);
