225abf9cda9cd08732e5261a58cb9d41e6b961a0
Aitor palomares <aitor.palomares@hmhco.com>
2017-08-17 08:42:23 +0100

    serviceWorker

--- src/index.jsx
+++ src/index.jsx
@@ -8,3 +8,11 @@ ReactDOM.render(
   <Main />,
   document.getElementById('root')
 );
+
+
+if (process.env.NODE_ENV === 'production') {
+	// cache all assets if browser supports serviceworker
+	if ('serviceWorker' in navigator && location.protocol === 'https:') {
+		navigator.serviceWorker.register('/service-worker.js');
+	}
+}
