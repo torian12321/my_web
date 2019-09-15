d861644ec2a93729dfc8d4397384bfc37db696d7
Aitor PC <aitorpalomares@hotmail.com>
2019-09-15 20:37:20 +0100

    custom ID on page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
-import { Menu } from "./components/ui";
+import { Menu } from "components/ui";
 import "./App.css";
 
 const App: React.FC = () => {
